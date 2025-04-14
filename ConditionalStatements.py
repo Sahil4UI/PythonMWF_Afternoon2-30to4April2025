b7df4ea273bafd99b22380ddc152992ec6f131d6
Sahil Kumar <54834362+Sahil4UI@users.noreply.github.com>
2025-04-14 15:45:03 +0530

    Create ConditionalStatements.py

---
+++ ConditionalStatements.py
@@ -0,0 +1,26 @@
+# if else
+
+# x = 12
+# if x%2==0:
+#     print("Even")
+# else:
+#     print("Odd")
+
+# ladder if else 
+# Write a program to find the largest b/w two numbers
+x = int(input("Enter No 1: "))
+y = int(input("Enter No 2: "))
+z = int(input("Enter No 3: "))
+
+# indentation
+if x>y and x>z:
+    print(f"{x} is largest")
+elif y>z:
+    print(f"{y} is largest")
+else:
+    print(f"{z} is largest")
+
+
+# Write a program to take 3 sides, if they form a traingle...state the traingle type?
+# equilateral/isoceles/scalene
+
