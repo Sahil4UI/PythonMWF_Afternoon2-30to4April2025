17a771d87b1e793f5a240118cf8b214c0a819933
Sahil Kumar <54834362+Sahil4UI@users.noreply.github.com>
2025-04-18 15:16:26 +0530

    Create loop exercise.py

---
+++ loop exercise.py
@@ -0,0 +1,42 @@
+# For Loop
+# here ending is n-1
+# range(start,end,step)
+# agar range - min to max , ending range is n-1
+# for i in range(1,11,2):
+#     print(i)
+
+
+# range - max to min .ending range is n+1
+# for i in range(10,0,-1):
+#     print(i)
+
+# for i in reversed(range(1,11)):
+#     print(i)
+
+# control flow statements - break and continue
+# break statement is used to transfer the control flow of 
+# program outside the loop
+
+# for i in range(1,10000000000000000000000000):
+#     if i==10:
+#         break
+#     print(i)
+
+# continue-skipping the current iteration - continue 
+# statement is used to transfet the control
+# flow of program to the starting of the loop
+
+# for i in range(1,11):
+#     if i%2==0:
+#         continue
+#     print(i)
+
+# Q1. print table of 2 in the following format
+# 2X1=2
+# 2X2=4
+# upto 2X10=20
+
+# Q2.write a program to find sum of first 10 natural numbers
+# Q3. Write a program to find factorial of a number
+# Q4.Write a program to find sum of digits of number
+# 123=1+2+3=6
