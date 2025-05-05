5fb96b88c150754e88bf384817b0f5881de7e45d
Sahil Kumar <54834362+Sahil4UI@users.noreply.github.com>
2025-05-05 15:20:12 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,8 +1,15 @@
 import webbrowser
+import os
+import glob
+import urllib.request as url  #sends request and return response
+import bs4 #bs4 is used to fetch data
+import random
+# but bs4 is not preinstalled ,you have to install it by using "pip install bs4" in cmd
 from datetime import datetime
 chat = True
 byeIntent = ["bye","bie","tata","see you","ttyl"]
 helloIntent = ["hey","hi"]
+
 while chat:
     msg = input("Enter Msg : ")
     if msg in helloIntent:
@@ -19,5 +26,25 @@ while chat:
         print(dt.strftime("%d-%m-%y,%a"))    
     elif msg.startswith("open"):
         webbrowser.open(msg.split()[-1]+".com")
+    elif "play" in msg:
+        musicList = glob.glob("*.mp3")
+        # for i in range(len(musicList)):
+        #     print(i+1 , musicList[i])
+        # choice = int(input("Enter music no from above : "))
+        # os.startfile(musicList[choice])
+        os.startfile(random.choice(musicList))
+        
+        
+    elif "news" in msg:
+        path = "https://indianexpress.com/section/"
+        choice = input("Press 1 for Politics\nPress 2 for Business\nPress 3 for Sports")
+        if choice == "1":
+            path += "political-pulse"
+            response = url.urlopen(path)
+            data = bs4.BeautifulSoup(response,features="html.parser")
+            h2_list = data.findAll("h2",class_="title")
+            for item in h2_list:
+                print(item.text)
+                print("\n***************************************\n")
     else:
         print("Sorry I didn't understand")
